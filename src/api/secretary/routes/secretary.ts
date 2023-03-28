9053b9b5094f9acc195681e4e865c638d1ae9133
Zack <zecharyw@gmail.com>
2023-03-29 01:35:26 +0800

    Added Page Secretary

---
+++ src/api/secretary/routes/secretary.ts
@@ -0,0 +1,7 @@
+/**
+ * secretary router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::secretary.secretary');
---
+++ src/api/secretary/services/secretary.ts
@@ -0,0 +1,7 @@
+/**
+ * secretary service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::secretary.secretary');
