000e7671bd56f232e55dd7a81fed5cc571a0dea6
Zack <zecharyw@gmail.com>
2023-05-11 06:03:01 +0800

    Update Lang

---
+++ config/plugins.ts
@@ -0,0 +1,14 @@
+// export default ({ env }) => ({
+//   // ...
+//   email: {
+//     config: {
+//       provider: "sendmail", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+//       settings: {
+//         defaultFrom: "info@clgcpa.com",
+//         defaultReplyTo: "info@clgcpa.com",
+//         testAddress: "zecharyw@gmail.com",
+//       },
+//     },
+//   },
+//   // ...
+// });
